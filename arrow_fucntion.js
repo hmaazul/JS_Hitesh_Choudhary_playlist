156b0e99502ba922c11b884623ebe87bc630c9a4
Maazul Haque <hmaazul25@gmail.com>
2024-04-14 13:02:07 +0530

    arrow fucntion commit

--- arrow_fucntion.js
+++ arrow_fucntion.js
@@ -5,7 +5,7 @@ const user = {
     username: "Maaz",
     age : 24,
     welcomeMessage: function(){
-        console.log(`${this.username} welcome to our website`);   // here this is used to access the context of curly braces in user object
+        console.log(`${this.username} welcome to our website`);   // here this is used to access the current context of curly braces in user object
         console.log(this);  // Output -> {
                             //  username: 'Maaz',
                             //  age: 24,
@@ -17,3 +17,5 @@ user.welcomeMessage()
 
 user.username = "Ahmad"
 user.welcomeMessage()
+
+console.log(this);  // {} -> it gives empty object in node environment as in curent context there is no global object but in case of browser it gives window objects as it's global object
