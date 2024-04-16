95251cc0c487e5595e1229a499d95c1cbdc1a094
Maazul Haque <hmaazul25@gmail.com>
2024-04-16 11:35:13 +0530

    arrow_function done

--- arrow_fucntion.js
+++ arrow_fucntion.js
@@ -1,5 +1,7 @@
 // ++++++++++++++++++++ Use of this keyword ++++++++++++++++++++ //
-// -> this is used for the context in which it's applied
+// -> this is used to refer the current context in which it's applied
+// -> this only works for objects but not for functions
+
 
 const user = {
     username: "Maaz",
@@ -13,9 +15,52 @@ const user = {
                             //  } 
     }
 }
-user.welcomeMessage()
+// user.welcomeMessage()
 
-user.username = "Ahmad"
-user.welcomeMessage()
+// user.username = "Ahmad"
+// user.welcomeMessage()
 
-console.log(this);  // {} -> it gives empty object in node environment as in curent context there is no global object but in case of browser it gives window objects as it's global object
+// console.log(this);  // {} -> it gives empty object in node environment as in curent context there is no global object but in case of browser it gives window objects as it's global object
+
+
+// +++++++ this keyword in functions +++++++++ //
+// 
+
+function checkThis(){
+    let username = "maaz"
+    console.log(this.username);   // undefined   => this doesn't work inside functions
+}
+
+// checkThis()
+
+const chai = function(){
+    let username = "maaz"
+    console.log(this.username);  //undefined
+}
+// chai() 
+
+// same for arrow function
+
+
+// ++++++++++++++++++++++++++++++ Arrow Function ++++++++++++++++++++++++++++++++++ //
+
+// const addTwo = (num1, num2) => {
+//     return num1+num2
+// }
+
+
+// ++++++ Implicit Return -> means we don't need to write return keyword...
+
+// const addTwo = (num1,num2) => num1+num2
+
+// const addTwo = (num1,num2) => (num1+num2)
+
+// console.log(addTwo(4,5));
+
+//return object from function -> use paranthesis () for that
+const returnObject = () =>  ({username: "Ahmad"})
+
+// console.log(returnObject());
+
+// ---------------Conclusion
+// if we use curly braces then we need to write return keyword but not in case of paranthesis
